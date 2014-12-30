2b94e9ec06a0bf8b6e6218a9e4bfc2be7d6c7b34
Jannis Achstetter <kripton@kripserver.net>
2014-12-30 20:48:21 +0100

    UI mockup for videoplayer

--- videoplayer.cpp
+++ videoplayer.cpp
@@ -6,6 +6,15 @@ VideoPlayer::VideoPlayer(QWidget *parent) :
     ui(new Ui::VideoPlayer)
 {
     ui->setupUi(this);
+    ui->playButton->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
+    ui->playButton->setIconSize(QSize(32, 32));
+    ui->pauseButton->setIcon(style()->standardIcon(QStyle::SP_MediaPause));
+    ui->pauseButton->setIconSize(QSize(32, 32));
+    ui->stopButton->setIcon(style()->standardIcon(QStyle::SP_MediaStop));
+    ui->stopButton->setIconSize(QSize(32, 32));
+
+    ui->dirSelectButton->setIcon(style()->standardIcon(QStyle::SP_DialogOpenButton));
+    ui->dirSelectButton->setIconSize(QSize(32, 32));
 }
 
 VideoPlayer::~VideoPlayer()
